7f74feb0fe86cd69d361029a380cb51fc96c324c
JesterCharles <JesterCharles@gmail.com>
2023-07-03 15:04:49 -0400

    updated the findall test for implementation

--- src/test/java/com/revature/member/MemberServiceTestSuite.java
+++ src/test/java/com/revature/member/MemberServiceTestSuite.java
@@ -1,11 +1,48 @@
 package com.revature.member;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class MemberServiceTestSuite {
 
+    @Mock
+    MemberRepository memberRepository;
 
+    @InjectMocks
+    MemberService sut;
+
+    @Test
     public void test_givenFindAllMembers_ReturnAllAvailableMembers(){
+        // Arrange
+        List<Member> memberList = new ArrayList<>();
+        Member member1 = new Member();
+        Member member2 = new Member();
+        Member member3 = new Member();
 
+        memberList.add(member1);
+        memberList.add(member2);
+        memberList.add(member3);
+        when(memberRepository.findAll()).thenReturn(memberList);
+
+
+        // Act
+        int actualSize = sut.findAll().size();
+        verify(memberRepository, times(1)).findAll();
+        verifyNoMoreInteractions(memberRepository);
+
+        // Assert
+        Assertions.assertEquals(3, actualSize);
     }
 
 
